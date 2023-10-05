4b50f0d311907eb2f82882463e2ccb85b04398d7
Mairaj Pirzada <thisismairaj@gmail.com>
2023-10-05 14:47:33 +0500

    more of JSX and following CodingAddict's tutorial series

--- CodingAddict/src/Book.jsx
+++ CodingAddict/src/Book.jsx
@@ -1,9 +1,11 @@
 const Book = () => {
+  const title = "Interesting Facts For Curious Minds"
+  const author = "Benjamin Franklin"
   return (
     <article className="book">
       <img src="./images/book-1.jpg" />
-      <h3>The Last Kite</h3>
-      <h4>Benjamin Franklin</h4>
+      <h3>{title}</h3>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   )
 }
