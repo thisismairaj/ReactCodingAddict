5d73e4df076157146bd042186ff22b9a5c0c39bc
Mairaj Pirzada <thisismairaj@gmail.com>
2023-10-05 16:03:11 +0500

    unique key issue

--- CodingAddict/src/Booklist.jsx
+++ CodingAddict/src/Booklist.jsx
@@ -1,31 +1,30 @@
 import Book from "./Book"
 
-const firstBook = {
-  author: "Jordan Moore",
-  title: "Interesting Facts For Curious Minds",
-  img: "./images/book-1.jpg",
-}
-
-const secondBook = {
-  author: "James Clear",
-  title: "Atomic Habits",
-  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
-}
+const books = [
+  {
+    author: "Jordan Moore",
+    title: "Facts For Curious Minds",
+    img: "./images/book-1.jpg",
+  },
+  {
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+  },
+  {
+    author: "Quaid e Azam",
+    title: "Pakistan Independence",
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg",
+  },
+]
 
 const Booklist = () => {
   return (
     <section className="booklist">
-      <h2>Amazon Best Selling Books List</h2>
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      />
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
+      {books.map((book, index) => {
+        const { title, author, img } = book
+        return <Book key={index} title={title} author={author} img={img} />
+      })}
     </section>
   )
 }
